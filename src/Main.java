f734de010d11a69c7737076c968a527340bd3285
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-16 13:52:58 -0400

    adding comments

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Scanner sc = new Scanner (System.in);
         ArrayList <Gift> options = new ArrayList<>();
 
+        // create array list of gift objects
         options.add(new Gift("Jewelry","Female",
                 "High", "adult"));
         options.add(new Gift("Weekend Getaway","Female",
@@ -31,6 +32,8 @@ public class Main {
         options.add(new Gift("Playstation","Male",
                 "Medium","child"));
 
+        // prompt user for input
+        
         System.out.println("Enter your gender");
         String userGender = sc.next();
 
@@ -40,6 +43,7 @@ public class Main {
         System.out.println("Enter the age of the person-child or teen or adult?");
         String userAge = sc.next();
 
+        // add gift suggestions to this list if anything matches
         ArrayList<String> giftSuggestions = new ArrayList<>();
 
         for (Gift gift1:options){
@@ -51,6 +55,7 @@ public class Main {
              }
         }
 
+        // if the suggestions list is empty, get user a gift certificate
         if(giftSuggestions.isEmpty()){
             System.out.println("You should get them a gift certificate");
         }
